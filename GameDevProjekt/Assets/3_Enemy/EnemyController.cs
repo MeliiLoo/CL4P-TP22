22c2daeaa7093ce2d08521b6dbff9e5fe1872cb1
MuriL105 <45102561+MuriL105@users.noreply.github.com>
2018-11-25 22:26:37 +0100

    Muriel
    Anpassung Enemy

---
+++ GameDevProjekt/Assets/3_Enemy/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+private Vector2 launchPos;
+[SerializeField]
+private Launcher ls;
+[SerializeField]
+private GameObject Body1;
+[SerializeField]
+private GameObject Body2;
+
+    void Start ()
+    {
+        // Hier bekommen wir die Position 
+		launchPos = GameObject.Find("Launcher").transform.position;
+        PlayEnemy1();
+		PlayEnemy2();
+    }
+
+    // Vereinfachung
+    void FixedUpdate()
+    {
+
+    }
+
+public void PlayEnemy1 ()
+{
+            Instantiate(Body1,
+            launchPos,
+            Quaternion.identity);
+}
+public void PlayEnemy2 ()
+{
+            Instantiate(Body2,
+            launchPos,
+            Quaternion.identity);
+}
+
+}
+
---
+++ GameDevProjekt/Assets/3_Enemy/EnemySkills.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkills : MonoBehaviour {
+
+[SerializeField]
+private int Health;
+[SerializeField]
+private GameObject Weapon;
+[SerializeField]
+private float speed;
+[SerializeField]
+private Rigidbody2D rigi;
+private GameObject body;
+
+private Vector3 startPos;
+private Vector3 newPos;
+
+	void Start () {
+
+	startPos = transform.position;
+	
+	}
+	
+	void Update () {
+
+	// automatisches hin und her laufen	
+	newPos = startPos;
+	newPos.x = newPos.x + Mathf.PingPong (Time.time * speed, 6) - 3;
+	transform.position = newPos;
+
+	// Wenn Lebenspunkte aufgebraucht
+	if(Health<1){
+			//destroy(GameObject, 1f);
+		}
+}
+
+void OnCollisionEnter2D(Collision2D collision)
+    {
+            if(Health>0)
+			Health-=1;
+
+        }
+
+public void shoot(){
+
+}
+
+public void kill(){
+
+}
+
+
+}

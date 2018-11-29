18a63fe7362ca9710915fa27b2451df88e4b0b5a
MuriL105 <45102561+MuriL105@users.noreply.github.com>
2018-11-29 12:22:07 +0100

    Muriel
    neue Sachen

--- GameDevProjekt/Assets/3_Enemy/EnemyController.cs
+++ GameDevProjekt/Assets/3_Enemy/EnemyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour {
 
@@ -14,10 +15,17 @@ private GameObject Body2;
 
     void Start ()
     {
+        Scene scene = SceneManager.GetActiveScene();
         // Hier bekommen wir die Position 
 		launchPos = GameObject.Find("Launcher").transform.position;
+
+        // Anhand Scene entscheiden, welchen Level
+        if(scene.name == "Scene_1"){
         PlayEnemy1();
+        }
+        if(scene.name == "Scene_2"){
 		PlayEnemy2();
+        }
     }
 
     // Vereinfachung
--- GameDevProjekt/Assets/3_Enemy/EnemySkills.cs
+++ GameDevProjekt/Assets/3_Enemy/EnemySkills.cs
@@ -13,42 +13,43 @@ private float speed;
 [SerializeField]
 private Rigidbody2D rigi;
 private GameObject body;
-
 private Vector3 startPos;
 private Vector3 newPos;
 
 	void Start () {
+	rigi = gameObject.AddComponent<Rigidbody2D>();
+	startPos = transform.position;	
 
-	startPos = transform.position;
-	
 	}
 	
 	void Update () {
-
 	// automatisches hin und her laufen	
 	newPos = startPos;
 	newPos.x = newPos.x + Mathf.PingPong (Time.time * speed, 6) - 3;
 	transform.position = newPos;
 
 	// Wenn Lebenspunkte aufgebraucht
-	if(Health<1){
-			//destroy(GameObject, 1f);
+	if(1 > Health){
+			kill();
 		}
 }
 
-void OnCollisionEnter2D(Collision2D collision)
-    {
-            if(Health>0)
-			Health-=1;
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(Health > 0)
+			Health-=1;
         }
+}
 
 public void shoot(){
 
 }
 
 public void kill(){
-
+Destroy(rigi,1f);
 }
 
 

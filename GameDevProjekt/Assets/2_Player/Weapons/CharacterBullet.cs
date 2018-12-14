f77dba4bb0c6fdf78b9f13d68c57c9d8020ed5d3
MeliiLoo <44904385+MeliiLoo@users.noreply.github.com>
2018-12-14 13:45:16 +0100

    .

---
+++ GameDevProjekt/Assets/2_Player/Weapons/CharacterBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBullet : MonoBehaviour {
+
+
+    [SerializeField]
+    private float speed = 15f;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private GameObject bulletImpact;
+
+    // Use this for initialization
+    void Start () {
+        rb.velocity = transform.right * speed;
+		
+	}
+	
+	void OnTriggerEnter2D (Collider2D hit) {
+
+        // if (EnemyController != nul) {
+
+        //    EnemyController.TakeDamage(damage)
+        // }
+        Instantiate(bulletImpact, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+	}
+}

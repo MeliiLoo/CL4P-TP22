504ed6ecea9f21b6137a438739ce32191f04c523
MuriL105 <45102561+MuriL105@users.noreply.github.com>
2018-12-16 09:47:32 +0100

    mh

---
+++ GameDevProjekt/Assets/08_Menu/ShowHighscore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHighscore : MonoBehaviour {
+
+public Text timeH;
+public Text scoreH;
+
+	// Use this for initialization
+	void Start () {
+
+		// Punkte aus PlayerPrefs holen und an Text übergeben
+			scoreH.text = PlayerPrefs.GetInt("Points",0) + "  Points".ToString();
+            timeH.text = PlayerPrefs.GetFloat("Time",0).ToString();
+	}
+
+}
